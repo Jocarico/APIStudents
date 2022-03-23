6f32950fc760a8c2275aee4697e7cf3eabda9f6b
Jonatan Rico <a6519150034@utch.edu.mx>
2022-03-23 11:31:46 -0700

    Changes on main.py code

--- app/main.py
+++ app/main.py
@@ -17,6 +17,16 @@ post_students_args.add_argument("image", type=str, help="ERROR you need to add i
 post_students_args.add_argument("group", type=str, required=False)
 post_students_args.add_argument("career", type=str, required=False)
 
+
+patch_students_args = reqparse.RequestParser()
+
+patch_students_args.add_argument("id", type=int, help="ERROR id value need to be an integer",required=False)
+patch_students_args.add_argument("first_name", type=str, help="ERROR first_name is required",required=False)
+patch_students_args.add_argument("last_name", type=str, help="ERROR last_name is required",required=False)
+patch_students_args.add_argument("image", type=str, help="ERROR you need to add image URL",required=False)
+patch_students_args.add_argument("group", type=str, required=False)
+patch_students_args.add_argument("career", type=str, required=False)
+
 class Test(Resource):
 
     def get(self):
@@ -71,10 +81,26 @@ class Student(Resource):
         return jsonify(args)
     
     def patch(self):
-        pass
+        student = self.abort_if_not_exist(id)
+        args = patch_students_args.parse_args()
 
-    def delete(self):
-        pass
+        database.db.students.update_one(
+            {'id': id},
+            {'$set': {
+                'id': args['id'],
+                'first_name': args['first_name'],
+                'last_name': args['last_name'],
+                'image': args['image'],
+                'group': args['group'],
+                'career': args['career'],
+            }}
+        )
+
+    def delete(self,id):
+        student = self.abort_if_not_exist(id)
+        database.db.students.delete_one({'id':id})
+        del student['_id']
+        return jsonify({'deleted': student})
 
     def abort_id_id_exist(self,id):
         if database.db.students.find_one({'id':id}):
