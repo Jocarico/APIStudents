e687bda94c36dd0cb29bd27426e268676789f2c2
Jonatan Rico <a6519150034@utch.edu.mx>
2022-03-25 18:48:59 -0700

    db_config directory changed

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from  flask_restful import Api, Resource, reqparse, abort
 from flask_cors import CORS
 # from flask_pymongo import pymongo
-import db_config as database
+import app.db_config as database
 
 app = Flask(__name__)
 api = Api(app)
